6874c86815c814c768b34ca1c7ae431d7c68285d
Renato <renatomaceneto@gmail.com>
2020-09-28 22:25:48 -0300

    almost ready

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.15
 require (
 	cloud.google.com/go v0.66.0
 	github.com/bwmarrin/discordgo v0.22.0
+	github.com/jonas747/dca v0.0.0-20200609191102-fe85ccf0947a
+	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
 	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
 	golang.org/x/sys v0.0.0-20200928205150-006507a75852 // indirect
 	google.golang.org/api v0.32.0 // indirect
--- main.go
+++ main.go
@@ -2,29 +2,51 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	"github.com/jonas747/dca"
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
+var voiceChannel *discordgo.VoiceConnection
 var buffer = make([][]byte, 0)
 
 func main() {
-	discord, err := discordgo.New("Bot " + "NTk1MzcwNjIyNzcxNjU4Nzcy.XRqAGQ.psJ17thR8hudrbWtUx-vVk3vcfE")
+	fmt.Println("Hello")
+	token := os.Getenv("TOKEN")
+	fmt.Println(token)
+
+	discord, err := discordgo.New("Bot " + token)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	discord.AddHandler(ready)
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	discord.AddHandler(requestToJoin)
+
+	discord.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates)
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 
 	// Instantiates a client.
 	ctx := context.Background()
@@ -38,25 +60,46 @@ func main() {
 	for scanner.Scan() {
 		text := scanner.Text()
 
-		go textToSpeech(ctx, text, client)
+		sound, err := textToSpeech(ctx, text, client)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buff := bytes.NewBuffer(sound)
+		err = dcaEncode(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = loadSound()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		filename := "output.mp3"
+		err = ioutil.WriteFile(filename, sound, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Audio content written to file: %v\n", filename)
+
+		playSound(voiceChannel)
 	}
 
+	discord.Close()
+
 }
 
-func ready(s *discordgo.Session, event *discordgo.Ready) {
+func textToSpeech(ctx context.Context, text string, client *texttospeech.Client) ([]byte, error) {
 
-	// Set the playing status.
-	fmt.Println("The bot is ready")
-}
-
-func textToSpeech(ctx context.Context, text string, client *texttospeech.Client) error {
-
+	fmt.Println("calling tts api")
 	// Perform the text-to-speech request on the text input with the selected
 	// voice parameters and audio file type.
 	req := texttospeechpb.SynthesizeSpeechRequest{
 		// Set the text input to be synthesized.
 		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Hello, World!"},
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
 		},
 		// Build the voice request, select the language code ("en-US") and the SSML
 		// voice gender ("neutral").
@@ -72,28 +115,18 @@ func textToSpeech(ctx context.Context, text string, client *texttospeech.Client)
 
 	resp, err := client.SynthesizeSpeech(ctx, &req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	// The resp's AudioContent is binary.
-	filename := "output.mp3"
-	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Audio content written to file: %v\n", filename)
-	return nil
+	return resp.AudioContent, nil
 }
 
 // playSound plays the current buffer to the provided channel.
-func playSound(s *discordgo.Session, guildID, channelID string, sound []byte) (err error) {
-
-	// Join the provided voice channel.
-	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
-	if err != nil {
-		return err
+func playSound(vc *discordgo.VoiceConnection) {
+	if vc == nil {
+		return
 	}
+	fmt.Println("Playing sound")
 
 	// Sleep for a specified amount of time before playing the sound
 	time.Sleep(250 * time.Millisecond)
@@ -101,20 +134,117 @@ func playSound(s *discordgo.Session, guildID, channelID string, sound []byte) (e
 	// Start speaking.
 	vc.Speaking(true)
 
-	// Send the buffer data.
-	vc.OpusSend <- sound
-	// for _, buff := range buffer {
-	// 	vc.OpusSend <- buff
-	// }
+	for _, buff := range buffer {
+		vc.OpusSend <- buff
+	}
 
 	// Stop speaking
 	vc.Speaking(false)
 
 	// Sleep for a specificed amount of time before ending.
 	time.Sleep(250 * time.Millisecond)
+}
 
-	// Disconnect from the provided voice channel.
-	vc.Disconnect()
+func requestToJoin(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	fmt.Println(m.Content)
+	// check if the message is "!airhorn"
+	if strings.HasPrefix(m.Content, "!join") {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+
+		// Find the channel that the message came from.
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			log.Println("Could not find channel.", err)
+			// Could not find channel.
+			return
+		}
+
+		// Find the guild for that channel.
+		g, err := s.State.Guild(c.GuildID)
+		if err != nil {
+			log.Println("Could not find guild", err)
+			// Could not find guild.
+			return
+		}
+
+		// Look for the message sender in that guild's current voice states.
+		for _, vs := range g.VoiceStates {
+			fmt.Println("userID", vs.UserID, "authorID", m.Author.ID)
+
+			if vs.UserID == m.Author.ID {
+				voiceChannel, err = s.ChannelVoiceJoin(g.ID, vs.ChannelID, false, true)
+				if err != nil {
+					log.Println("kkkk", err)
+				}
+			}
+		}
+	}
+}
+
+func dcaEncode(data io.Reader) error {
+	// Encoding a file and saving it to disk
+
+	encodeSession, err := dca.EncodeMem(data, dca.StdEncodeOptions)
+	if err != nil {
+		return err
+	}
+	// Make sure everything is cleaned up, that for example the encoding process if any issues happened isnt lingering around
+	defer encodeSession.Cleanup()
+
+	output, err := os.Create("output.dca")
+	if err != nil {
+		return err
+	}
+
+	io.Copy(output, encodeSession)
 	return nil
+}
+
+// loadSound attempts to load an encoded sound file from disk.
+func loadSound() error {
+
+	file, err := os.Open("output.dca")
+	if err != nil {
+		fmt.Println("Error opening dca file :", err)
+		return err
+	}
+
+	var opuslen int16
+
+	for {
+		// Read opus frame length from dca file.
+		err = binary.Read(file, binary.LittleEndian, &opuslen)
+
+		// If this is the end of the file, just return.
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			err := file.Close()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		// Read encoded pcm from dca file.
+		InBuf := make([]byte, opuslen)
+		err = binary.Read(file, binary.LittleEndian, &InBuf)
+
+		// Should not be any end of file errors
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			return err
+		}
+
+		// Append encoded pcm data to the buffer.
+		buffer = append(buffer, InBuf)
+	}
 }
